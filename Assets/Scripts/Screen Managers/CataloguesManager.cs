c9d6731f7c901a2afeb6fa520061c74afb7acd51
Marcus <snickersmarsbounty@gmail.com>
2024-06-22 19:21:23 +0200

    Grundgerüst steht, funktionalität fehlt noch

--- Assets/Scripts/Screen Managers/CataloguesManager.cs
+++ Assets/Scripts/Screen Managers/CataloguesManager.cs
@@ -2,27 +2,31 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class CataloguesManager : MonoBehaviour
 {
 
     [SerializeField] private TMP_Dropdown catalogueSelection;
     [SerializeField] private TMP_Dropdown questionSelection;
-    [SerializeField] private UnityEngine.UI.Button QButton;
-    [SerializeField] private UnityEngine.UI.Button AButton1;
-    [SerializeField] private UnityEngine.UI.Button AButton2;
-    [SerializeField] private UnityEngine.UI.Button AButton3;
-    [SerializeField] private UnityEngine.UI.Button AButton4;
-    private TextMeshProUGUI QButton_Label;
-    private TextMeshProUGUI AButton1_Label;
-    private TextMeshProUGUI AButton2_Label;
-    private TextMeshProUGUI AButton3_Label;
-    private TextMeshProUGUI AButton4_Label;
+    [SerializeField] private Button questionButton;
+    [SerializeField] private Button[] answerButtons = new Button[4];
+
+    private TextMeshProUGUI questionButtonLabel;
+    private List<TextMeshProUGUI> answerButtonLabels = new List<TextMeshProUGUI>();
 
     CatalogueTable catalogueTable;
-
-
+    Catalogue currentCatalogue;
     List<Catalogue> catalogues;
+
+    // This "should" never be needed... but. better safe than sorry.
+    bool questionAddRecursionFailsafe = false;
+    bool invalidStart = true;
+
+    bool useTmpCatalogue = false; // Use the Global.tmpCatalogue instead of the ones from the DB
+    int questionIndexAfterReset = -1;
+    int catalogueIndexAfterReset = -1;
+
 
     void Start()
     {
@@ -30,78 +34,210 @@ public class CataloguesManager : MonoBehaviour
         {
             print("ERROR [CataloguesManager.cs:Start()]: Dont use this script in any scene other than \"" + SceneManager.GetActiveScene().name + "\"!");
         }
-        QButton_Label = QButton.GetComponentInChildren<TextMeshProUGUI>();
-        AButton1_Label = AButton1.GetComponentInChildren<TextMeshProUGUI>();
-        AButton2_Label = AButton2.GetComponentInChildren<TextMeshProUGUI>();
-        AButton3_Label = AButton3.GetComponentInChildren<TextMeshProUGUI>();
-        AButton4_Label = AButton4.GetComponentInChildren<TextMeshProUGUI>();
-        catalogueTable = SQLiteSetup.Instance.catalogueTable;
-        catalogues = catalogueTable.FindAllCatalogues();
-        SetContents();
+
+        // Get components for questionButton
+        questionButtonLabel = questionButton.GetComponentInChildren<TextMeshProUGUI>();
+
+        // Get components for answer buttons and add them to the lists
+        foreach (Button button in answerButtons)
+            answerButtonLabels.Add(button.GetComponentInChildren<TextMeshProUGUI>());
+
+        try
+        {
+            catalogueTable = SQLiteSetup.Instance.catalogueTable;
+            catalogues = catalogueTable.FindAllCatalogues();
+            invalidStart = false; // catalogue could be loaded -> all fine from here on out
+            SetContents();
+        }
+        catch
+        {
+            print("ERROR: You propably started this screen directly. Start \"Home\" and navigate here instead.");
+        }
+
     }
 
     private void SetContents()
     {
-        catalogueSelection.ClearOptions();
-        List<TMP_Dropdown.OptionData> options = new();
-        for (int i = 0; i < catalogues.Count; i++)
-        {
-            Catalogue catalogue = catalogues[i];
-            options.Add(new(catalogue.name));
-        }
-        if (options.Count == 0)
-        {
-            options.Add(new("Nicht verfügbar"));
-        }
-        catalogueSelection.AddOptions(options);
-        CatalogueSelectionChangedEvent(); // This will update QuestionSelection
+        if (invalidStart) return;
+        // This cascades through every subsequent element and updates everything.
+        CatalogueSelectionUpdate();
     }
 
     public void CatalogueSelectionChangedEvent()
     {
-        questionSelection.ClearOptions();
-        List<TMP_Dropdown.OptionData> options = new();
-        if (catalogues.Count != 0)
-        {
-            Catalogue catalogue = catalogueTable.FindCatalogueByName(catalogueSelection.options[catalogueSelection.value].text);
-            for (int i = 0; i < catalogue.questions.Count; i++)
-            {
-                options.Add(new(catalogue.questions[i].text));
-            }
-        }
-        if (options.Count == 0)
-        {
-            options.Add(new("-"));
-        }
-        questionSelection.AddOptions(options);
-        QuestionSelectionChangedEvent();
-    }
+        if (invalidStart) return;
+        questionIndexAfterReset = -1;
+        string selectedCatalogueName = catalogueSelection.options[catalogueSelection.value].text;
+        useTmpCatalogue = selectedCatalogueName == "Neu";
 
+        if(useTmpCatalogue)
+            currentCatalogue = Global.tmpCatalogue;
+        else
+            currentCatalogue = catalogueTable.FindCatalogueByName(selectedCatalogueName);
+
+        QuestionSelectionUpdate();
+  
+    }
+ 
     public void QuestionSelectionChangedEvent()
     {
-        if (catalogues.Count == 0)
+        if (invalidStart) return;
+        catalogueIndexAfterReset = -1;
+        string selectedQuestionName = questionSelection.options[questionSelection.value].text;
+
+        if (selectedQuestionName == "Neu" && questionAddRecursionFailsafe == false)
         {
-            print("ERROR [CataloguesManager.cs:QuestionSelectionChangedEvent()]: Wir benötigen mehr Fragenkataloge, Milord.");
-            return;
+            // Add the Question named "Frage N"
+            Answer defaultAnswer = new(-1, "", -1, false);
+            List<Answer> answers = new List<Answer>();
+            for (int i = 0; i < 4; i++)
+            {
+                defaultAnswer.text = "Antwort " + (i + 1).ToString();
+                answers.Add(defaultAnswer);
+            }
+
+            // Count how many "Neue Frage" there are to prevent duplicates
+            int newQuestionCounter = 0;
+            foreach(Question question in currentCatalogue.questions)
+            {
+                if(question.text[.."Frage".Length] == "Frage")
+                    newQuestionCounter++;
+            }
+
+            Question newQuestion = new(-1, "Frage" + (newQuestionCounter + 1).ToString(), -1, answers);
+
+            currentCatalogue.questions.Add(newQuestion);
+            questionIndexAfterReset = currentCatalogue.questions.IndexOf(newQuestion);
+
+            // This can result in an infinite recursion if the question name at the new questionIndex is somehow "Neu"
+            // Just to be safe -> disable recursion for the following call.
+            questionAddRecursionFailsafe = true; 
+            QuestionSelectionUpdate();
         }
-        if (catalogues.Count < catalogueSelection.value)
+        questionAddRecursionFailsafe = false;
+
+        QuestionAndAnswersSetContents();
+
+    }
+
+    public void EventCatalogueRenameButton()
+    {
+        if (invalidStart) return;
+        print("EventCatalogueRenameButton");
+        // Prompts the user for a name.
+        // Sets the name of the current catalogue.
+        // If the catalogue is NOT the tmpCatalogue: Updates DB with a new name for this catalogue.
+        // Else: All fine.
+    }
+
+    public void EventCatalogueAddButton()
+    {
+        if (invalidStart) return;
+        print("EventCatalogueAddButton");
+        // Promts the user for a name.
+        // Adds the tmpCatalogue to the DB with the name given by the user
+        // Resets the tmpCatalogue to default.
+    }
+
+    public void EventCatalogueDeleteButton()
+    {
+        if (invalidStart) return;
+        print("EventCatalogueDeleteButton");
+        // Prompts the user if they are really sure what they are doing.
+        // Moves the currently selected, existing catalogue (not for the tmpCatalogue) to the recycle bin.
+        // Finally deleting maybe on the settings screen?
+    }
+
+    public void EventQuestionRenameButton()
+    {
+        if (invalidStart) return;
+        print("EventQuestionRenameButton");
+        // Promts the user for a name.
+        // This name is given to the currently selected question.
+        // If the question is from tmpCatalogue, no DB-Update required.
+        // Else DB-Update required.
+    }
+
+    public void EventQuestionDeleteButton()
+    {
+        if (invalidStart) return;
+        print("EventQuestionDeleteButton");
+        // Prompts the user if they are really sure what they are doing.
+        // Deletes the Question from the currently selected catalogue.
+        // If the question is from tmpCatalogue, no DB-Update required.
+        // Else DB-Update required.
+    }
+
+    public void EventQuestionEditButton()
+    {
+        if (invalidStart) return;
+        print("EventQuestionEditButton");
+        // Prompts the user with a text-input-field where the current question is written out and editable.
+        // Closing the prompt will update the question text.
+        // If the question is from tmpCatalogue, no DB-Update required.
+        // Else DB-Update required.
+    }
+
+    public void EventAnswerEditButton(int answerButtonIndex)
+    {
+        if (invalidStart) return;
+        print("EventAnswerEditButton " + answerButtonIndex.ToString());
+        // Prompts the user with a text-input-field where the selected answer is written out and editable.
+        // Closing the prompt will update the question text.
+        // If the question is from tmpCatalogue, no DB-Update required.
+        // Else DB-Update required.
+    }
+
+    private void QuestionAndAnswersSetContents()
+    {
+        if (invalidStart) return;
+        if (currentCatalogue.questions.Count == 0)
         {
-            print("ERROR [DropdownManager.cs.SetContents_QuestionAnswerButtons()]: Invalid Index for Fragenkatalognummer: " + catalogueSelection.value);
-            return;
+            // This should never happen. TODO (if a new question made, it "exists" already)
+            // If this ever occurs while editing -> file a report :)
+            questionButtonLabel.text = "-";
+            foreach (TextMeshProUGUI label in answerButtonLabels)
+                label.text = "-";
         }
-        if (catalogueTable.FindCatalogueById(Global.CurrentQuestionRound.catalogueIndex).questions.Count < questionSelection.value)
+        else
         {
-            print("ERROR [DropdownManager.cs.SetContents_QuestionAnswerButtons()]: Invalid Index for Fragennummer: " + catalogueSelection.value + " in Fragenkatalognummer: " + questionSelection.value);
-            return;
+            Question currentQuestion = currentCatalogue.questions[questionSelection.value];
+            questionButtonLabel.text = currentQuestion.text;
+            for(int i = 0; i < answerButtons.Length; i++)
+            {
+                answerButtonLabels[i].text = currentQuestion.answers[i].text;
+            }
         }
-        Catalogue catalogue = catalogueTable.FindCatalogueByName(catalogueSelection.options[catalogueSelection.value].text);
-        Question currentQuestion = catalogue.questions[questionSelection.value];
-        QButton_Label.text = currentQuestion.text;
-        AButton1_Label.text = currentQuestion.answers[0].text;
-        AButton1_Label.text = currentQuestion.answers[0].text;
-        AButton2_Label.text = currentQuestion.answers[1].text;
-        AButton3_Label.text = currentQuestion.answers[2].text;
-        AButton4_Label.text = currentQuestion.answers[3].text;
+    }
+
+    private void CatalogueSelectionUpdate()
+    {
+        if (invalidStart) return;
+        catalogueSelection.ClearOptions();
+        List<TMP_Dropdown.OptionData> options = new();
+        for (int i = 0; i < catalogues.Count; i++)
+            options.Add(new(catalogues[i].name));
+        options.Add(new("Neu")); // Option to add a new catalogue
+        catalogueSelection.AddOptions(options);
+        if (catalogueIndexAfterReset != -1)
+            catalogueSelection.value = catalogueIndexAfterReset;
+
+        CatalogueSelectionChangedEvent(); // Update everything that is affected by this update
+    }
+
+    private void QuestionSelectionUpdate()
+    {
+        if (invalidStart) return;
+        questionSelection.ClearOptions();
+        List<TMP_Dropdown.OptionData> options = new();
+        for (int i = 0; i < currentCatalogue.questions.Count; i++)
+            options.Add(new("Frage " + (i + 1).ToString()));
+        options.Add(new("Neu")); // Option to add a new question
+        questionSelection.AddOptions(options);
+        if(questionIndexAfterReset != -1)
+            questionSelection.value = questionIndexAfterReset;
+
+        QuestionSelectionChangedEvent(); // Update everything that is affected by this update
     }
 
 }
