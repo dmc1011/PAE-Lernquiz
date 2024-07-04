4a85d6f583d4edc1f47662d671df89b9b885fc72
HDingeldein <113993544+HDingeldein@users.noreply.github.com>
2024-07-04 21:37:09 +0200

    delete questions and answers on cascade

--- Assets/Scripts/SQLiteSetup.cs
+++ Assets/Scripts/SQLiteSetup.cs
@@ -26,6 +26,10 @@ public class SQLiteSetup : MonoBehaviour
             dbConnection = new SqliteConnection(dbConnectionString);
             dbConnection.Open();
 
+            IDbCommand enableForeignKeyCommand = dbConnection.CreateCommand();
+            enableForeignKeyCommand.CommandText = "PRAGMA foreign_keys = ON;";
+            enableForeignKeyCommand.ExecuteNonQuery();
+
             CreateTables();
 
             catalogueTable = new CatalogueTable(dbConnection);
@@ -83,14 +87,14 @@ public class SQLiteSetup : MonoBehaviour
                 Id INTEGER PRIMARY KEY AUTOINCREMENT,
                 CatalogueId INTEGER,
                 Text TEXT,
-                FOREIGN KEY(CatalogueId) REFERENCES Catalogue(Id)
+                FOREIGN KEY(CatalogueId) REFERENCES Catalogue(Id) ON DELETE CASCADE
             );
             CREATE TABLE IF NOT EXISTS Answer (
                 Id INTEGER PRIMARY KEY AUTOINCREMENT,
                 QuestionId INTEGER,
                 Text TEXT,
                 IsCorrect BOOLEAN,
-                FOREIGN KEY(QuestionId) REFERENCES Question(Id)
+                FOREIGN KEY(QuestionId) REFERENCES Question(Id) ON DELETE CASCADE
             );
         ";
         dbCommand.ExecuteNonQuery();
