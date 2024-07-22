15ee54ae56d36c85096538964705ac2b82b91374
Marcus <snickersmarsbounty@gmail.com>
2024-07-22 18:26:35 +0200

    background ueberarbeitet und fixes

--- Assets/Scripts/UI Element Managers/ButtonNavigation.cs
+++ Assets/Scripts/UI Element Managers/ButtonNavigation.cs
@@ -3,8 +3,25 @@ using UnityEngine.SceneManagement;
 
 public class ButtonNavigation : MonoBehaviour
 {
+    [SerializeField] private Background bg = null;
+    private string targetSceneName = "";
+
     public void LoadScene(string sceneName)
     {
-        SceneManager.LoadScene(sceneName);
+        targetSceneName = sceneName;
+        if (bg != null)
+        {
+            float timeNeeded = bg.TriggerEndSequence();
+            Invoke(nameof(LoadSceneInternal), timeNeeded);
+        }
+        else
+        {
+            LoadSceneInternal();
+        }
+    }
+
+    private void LoadSceneInternal()
+    {
+        SceneManager.LoadScene(targetSceneName);
     }
 }
