c273afb2e77ff5be99ca0c9d271b36b98ac68d79
dmc1011 <dmccarroll98@gmail.com>
2024-07-30 23:52:04 +0200

    Scroll View Fix

--- Assets/Scripts/Screen Managers/NewGameManager.cs
+++ Assets/Scripts/Screen Managers/NewGameManager.cs
@@ -1,5 +1,5 @@
 using System.Collections.Generic;
-using System.Linq;
+//using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -8,11 +8,11 @@ using UnityEngine.UI;
 public class NewGameManager : MonoBehaviour
 {
 
-    [SerializeField] private TMP_Dropdown catalogueSelection;
-    [SerializeField] private Button startLinearRound;
-    [SerializeField] private ButtonNavigation startLinearRoundNavigation;
-    [SerializeField] private Button startRandomRound;
-    [SerializeField] private ButtonNavigation startRandomRoundNavigation;
+    //[SerializeField] private TMP_Dropdown catalogueSelection;
+    //[SerializeField] private Button startLinearRound;
+    //[SerializeField] private ButtonNavigation startLinearRoundNavigation;
+    //[SerializeField] private Button startRandomRound;
+    //[SerializeField] private ButtonNavigation startRandomRoundNavigation;
 
     [SerializeField] private GameObject catalogueButtonPrefab;
     [SerializeField] private Transform buttonContainer;
@@ -27,20 +27,21 @@ public class NewGameManager : MonoBehaviour
         // Failsave
         if (SceneManager.GetActiveScene().name != "NewGame")
         {
-            print("ERROR [NewGameManager.cs:Start()]: Dont use this script in any scene other than \"" + SceneManager.GetActiveScene().name + "\"!");
+            print("ERROR [NewGameManager.cs:Start()]: Dont use this script in any scene other than 'NewGame'.");
             return;
         }
+
         catalogueTable = SQLiteSetup.Instance.catalogueTable;
         catalogues = catalogueTable.FindAllCatalogues();
         catalogueCount = catalogues.Count;
-        SetCatalogueButtons();
         gameMode = Global.CurrentQuestionRound.gameMode;
-        Debug.Log(gameMode);
+
+        SetCatalogueButtons();
     }
 
     private void SetCatalogueButtons()
     {
-        for (int i = 0; i < catalogues.Count; i++)
+        for (int i = 0; i < catalogueCount; i++)
         {
             GameObject catalogueButton = Instantiate(catalogueButtonPrefab, buttonContainer);
             TextMeshProUGUI buttonLabel = catalogueButton.GetComponentInChildren<TextMeshProUGUI>();
