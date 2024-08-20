c9f9d1f6e6e20fd2ac1dd62532713a94a8723f34
Marcus <snickersmarsbounty@gmail.com>
2024-08-20 14:48:07 +0200

    Achievement-Screen mit beispielen

--- Assets/Scripts/UI Element Managers/AchievementPopup.cs
+++ Assets/Scripts/UI Element Managers/AchievementPopup.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class AchievementPopup : MonoBehaviour
 {
     [SerializeField] private RectTransform popupTransform;
+    [SerializeField] private Image none;
     [SerializeField] private Image bronze;
     [SerializeField] private Image silver;
     [SerializeField] private Image gold;
@@ -20,16 +21,17 @@ public class AchievementPopup : MonoBehaviour
 
     public enum Grade
     {
-        Bronze, Silver, Gold
+        None, Bronze, Silver, Gold
     }
 
     AnimationMode animationMode = AnimationMode.Idle;
 
     Vector3 animation_time = new(1.0f, 5.0f, 1.0f); // Animationtime for "Start", "Wait", "End"
-    float current_animation_position = 0;
+    float currentAnimationPosition = 0;
     private Vector2 onScreenPosition;
     private Vector2 offScreenPositionLeft;
     private Vector2 offScreenPositionRight;
+    private bool startAnimation = false;
 
     private void Start()
     {
@@ -37,24 +39,32 @@ public class AchievementPopup : MonoBehaviour
         offScreenPositionLeft = new(-Screen.width, Screen.height * 0.4f);
         offScreenPositionRight = new(Screen.width, Screen.height * 0.4f);
         popupTransform.anchoredPosition = offScreenPositionLeft;
+        animationMode = AnimationMode.Idle;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(current_animation_position > 0)
+        if(startAnimation)
+        {
+            animationMode = AnimationMode.Start;
+            currentAnimationPosition = animation_time[0];
+            startAnimation = false;
+        }
+
+        if(currentAnimationPosition > 0)
         {
             switch (animationMode)
             {
                 case AnimationMode.Start:
                     {
-                        float t = 1 - current_animation_position / animation_time[0];
+                        float t = 1 - currentAnimationPosition / animation_time[0];
                         popupTransform.anchoredPosition = Vector2.Lerp(offScreenPositionLeft, onScreenPosition, Mathf.Pow(t, 2.5f));
-                        current_animation_position -= Time.deltaTime;
-                        if (current_animation_position <= 0)
+                        currentAnimationPosition -= Time.deltaTime;
+                        if (currentAnimationPosition <= 0)
                         {
                             popupTransform.anchoredPosition = onScreenPosition;
-                            current_animation_position = animation_time[1];
+                            currentAnimationPosition = animation_time[1];
                             animationMode = AnimationMode.Wait;
                         }
                     }
@@ -62,7 +72,7 @@ public class AchievementPopup : MonoBehaviour
 
                 case AnimationMode.Wait:
                     {
-                        float t = 1 - current_animation_position / animation_time[1];
+                        float t = 1 - currentAnimationPosition / animation_time[1];
                         float t4 = t * 4;
                         float t10 = t * 10;
 
@@ -87,11 +97,11 @@ public class AchievementPopup : MonoBehaviour
                             iconContainer.localScale = new(1, 1, 1);
                         }
 
-                        current_animation_position -= Time.deltaTime;
-                        if (current_animation_position <= 0)
+                        currentAnimationPosition -= Time.deltaTime;
+                        if (currentAnimationPosition <= 0)
                         {
                             iconContainer.rotation = Quaternion.Euler(0, 0, 0);
-                            current_animation_position = animation_time[2];
+                            currentAnimationPosition = animation_time[2];
                             animationMode = AnimationMode.End;
                         }
                     }
@@ -99,13 +109,13 @@ public class AchievementPopup : MonoBehaviour
 
                 case AnimationMode.End:
                     {
-                        float t = 1 - current_animation_position / animation_time[2];
+                        float t = 1 - currentAnimationPosition / animation_time[2];
                         popupTransform.anchoredPosition = Vector2.Lerp(onScreenPosition, offScreenPositionRight, Mathf.Pow(t, 2.5f));
-                        current_animation_position -= Time.deltaTime;
-                        if (current_animation_position <= 0)
+                        currentAnimationPosition -= Time.deltaTime;
+                        if (currentAnimationPosition <= 0)
                         {
                             popupTransform.anchoredPosition = offScreenPositionRight;
-                            current_animation_position = 0;
+                            currentAnimationPosition = 0;
                             animationMode = AnimationMode.Idle;
                             Destroy(gameObject);
                         }
@@ -118,15 +128,21 @@ public class AchievementPopup : MonoBehaviour
 
     public void Fire()
     {
-        current_animation_position = animation_time[0];
-        animationMode = AnimationMode.Start;
+        startAnimation = true;
     }
 
     public void SetData(Grade achievementGrade, string title, string text)
     {
+        none.gameObject.SetActive(achievementGrade == Grade.None);
         bronze.gameObject.SetActive(achievementGrade == Grade.Bronze);
         silver.gameObject.SetActive(achievementGrade == Grade.Silver);
         gold.gameObject.SetActive(achievementGrade == Grade.Gold);
+
+        if(achievementGrade == Grade.None)
+        {
+            title_element.color = Color.gray;
+            text_element.color = Color.gray;
+        }
         title_element.text = title;
         text_element.text = text;
     }
